55395fb88dc8c2d364d3d49c8514986c2ae0be89
ely67 <111973666+ely67@users.noreply.github.com>
2023-09-23 16:42:34 +0330

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,6 +6,7 @@ const networkConfig = {
     11155111: {
         name: "sepolia",
         ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+        usdtContract: "0xA1d7f71cbBb361A77820279958BAC38fC3667c1a",
     },
 }
 
