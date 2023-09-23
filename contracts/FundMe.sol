d770d592fa98a4d1d1dfb3d49f0e622298e0d0cf
ely67 <111973666+ely67@users.noreply.github.com>
2023-09-23 16:32:00 +0330

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.7;
 // 2. Imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./PriceConverter.sol";
 
 // 3. Interfaces, Libraries, Contracts
@@ -21,8 +23,11 @@ contract FundMe {
     uint256 public constant MINIMUM_USD = 50 * 10**18;
     address private immutable i_owner;
     address[] private s_funders;
+    address[] private s_usdtFunders;
     mapping(address => uint256) private s_addressToAmountFunded;
+    mapping(address => uint256) private s_addressUsdtFunded;//s_usdtFunds;
     AggregatorV3Interface private s_priceFeed;
+    IERC20 public usdtToken; // the ERC20 token contract for usdt
 
     // Events (we have none!)
 
@@ -43,10 +48,15 @@ contract FundMe {
     //// private
     //// view / pure
 
-    constructor(address priceFeed) {
+    constructor(address priceFeed, address _usdtTokenAddress) {
         s_priceFeed = AggregatorV3Interface(priceFeed);
         i_owner = msg.sender;
+        usdtToken = IERC20(_usdtTokenAddress);
+    
     }
+
+    event funded(address indexed from, uint256 time, uint256 value);
+    event withdrawLog(address indexed from, uint256 time);
 
     /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
@@ -57,6 +67,24 @@ contract FundMe {
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         s_addressToAmountFunded[msg.sender] += msg.value;
         s_funders.push(msg.sender);
+        emit funded(msg.sender, block.timestamp, msg.value);
+    }
+
+    function fundUsdt(uint256 usdtAmount) public payable{
+        require(
+            usdtAmount >= MINIMUM_USD,
+            "You need to spend more USDT"
+        );
+
+        // Transfer USDT from the sender to the contract
+        require(
+            usdtToken.transferFrom(msg.sender, address(this), usdtAmount),
+            "USDT transfer failed"
+        );
+
+        s_addressUsdtFunded[msg.sender] += usdtAmount;
+        s_usdtFunders.push(msg.sender);
+        emit funded(msg.sender, block.timestamp, msg.value);
     }
 
     function withdraw() public onlyOwner {
@@ -73,6 +101,22 @@ contract FundMe {
         // payable(msg.sender).transfer(address(this).balance);
         (bool success, ) = i_owner.call{value: address(this).balance}("");
         require(success);
+        emit withdrawLog(msg.sender, block.timestamp);
+    }
+
+    function withdrawUsdt() public onlyOwner{
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_usdtFunders.length;
+            funderIndex++
+        ) {
+            address funder = s_usdtFunders[funderIndex];
+            s_addressUsdtFunded[funder] = 0;
+        }
+        s_usdtFunders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success);
+        emit withdrawLog(msg.sender, block.timestamp);
     }
 
     function cheaperWithdraw() public onlyOwner {
@@ -90,6 +134,23 @@ contract FundMe {
         // payable(msg.sender).transfer(address(this).balance);
         (bool success, ) = i_owner.call{value: address(this).balance}("");
         require(success);
+        emit withdrawLog(msg.sender, block.timestamp);
+    }
+
+    function cheaperUsdtWithdraw() public onlyOwner {
+        address[] memory usdtF = s_usdtFunders;
+         for (
+            uint256 funderIndex = 0;
+            funderIndex < usdtF.length;
+            funderIndex++
+        ) {
+            address funder = usdtF[funderIndex];
+            s_addressUsdtFunded[funder] = 0;
+        }
+        s_usdtFunders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success);
+        emit withdrawLog(msg.sender, block.timestamp);
     }
 
     /** @notice Gets the amount that an address has funded
@@ -104,12 +165,20 @@ contract FundMe {
         return s_addressToAmountFunded[fundingAddress];
     }
 
+    function getAddressUsdtFunded(address fundingUsdtAddress) public view returns (uint256) {
+        return s_addressUsdtFunded[fundingUsdtAddress];
+    }
+
     function getVersion() public view returns (uint256) {
         return s_priceFeed.version();
     }
 
     function getFunder(uint256 index) public view returns (address) {
         return s_funders[index];
+    }
+
+    function getUsdtFunders(uint256 index) public view returns (address) {
+        return s_usdtFunders[index];
     }
 
     function getOwner() public view returns (address) {
