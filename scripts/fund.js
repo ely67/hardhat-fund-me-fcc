ca93115c9fed098b604711379171fdbc1801a0c7
ely67 <111973666+ely67@users.noreply.github.com>
2023-10-12 13:01:07 +0330

    Update fund.js

--- scripts/fund.js
+++ scripts/fund.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat")
 async function main() {
   const FundMeFactory = await ethers.getContractFactory("FundMe")
   console.log("Deploying contract...")
-  const fundMe = await FundMeFactory.deploy("0x694AA1769357215DE4FAC081bf1f309aDC325306", "0xA1d7f71cbBb361A77820279958BAC38fC3667c1a")
+  const fundMe = await FundMeFactory.deploy("0x694AA1769357215DE4FAC081bf1f309aDC325306")
   await fundMe.deployed()
   console.log(`Deployed contract to: ${fundMe.address}`)
 }
