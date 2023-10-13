40ada194128e1a2201faad5f0377f67a643fe3a9
ely67 <elyassarjooghi6561@gmail.com>
2023-10-13 10:55:27 +0330

    Initial Commit

---
+++ project/scripts/fund.js
@@ -0,0 +1,23 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+  const fundMeFactory = await ethers.getContractFactory("fundMe")
+  console.log("Deploying contract...")
+  const fundMe = await fundMeFactory.deploy("0x694AA1769357215DE4FAC081bf1f309aDC325306")
+  await fundMe.deployed()
+  console.log(`Deployed contract to: ${fundMe.address}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
+
+
+
+
+
+
